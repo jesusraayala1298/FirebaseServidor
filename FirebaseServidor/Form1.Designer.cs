545e85b25e89c48e013a9a7417a6138bd824afcc
Jesus Ramirez Ayala <48115771+jesusraayala1298@users.noreply.github.com>
2021-06-03 23:37:19 -0500

    Agregar archivos de proyecto.

---
+++ FirebaseServidor/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace FirebaseServidor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMensaje = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(107, 43);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(280, 23);
+            this.txtTitulo.TabIndex = 0;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(203, 204);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 1;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Titulo:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mensaje:";
+            // 
+            // txtMensaje
+            // 
+            this.txtMensaje.Location = new System.Drawing.Point(107, 96);
+            this.txtMensaje.Multiline = true;
+            this.txtMensaje.Name = "txtMensaje";
+            this.txtMensaje.Size = new System.Drawing.Size(280, 102);
+            this.txtMensaje.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 311);
+            this.Controls.Add(this.txtMensaje);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtTitulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMensaje;
+    }
+}
+
---
+++ FirebaseServidor/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FirebaseAdmin;
+using FirebaseAdmin.Messaging;
+using Google.Apis.Auth.OAuth2;
+
+namespace FirebaseServidor
+{
+ 
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        private async Task btnPeticion_ClickAsync()
+        {
+            //CREDENCIALES PROPORCIONADAS POR FIREBASE
+            FirebaseApp.Create(new AppOptions()
+            {
+                Credential = GoogleCredential.FromFile("C:/Users/Jesus Ramirez Ayala/Downloads/fir-android-7686b-firebase-adminsdk-6i4e0-128d7be009.json")
+            });
+
+            //TOKEN DEL DISPOSITIVO PARA ENVIAR LA NOTIFICACION
+            var registrationToken = "cfCiQuv4RoaCVDh4wbeZRx:APA91bGUwLq62WPx22Z5Z9PK5ZwrqKuu_KeUNoQw6i-54yFcW8_pRJdvHArjhsDXo1HeLMptwZuioY4PGN8aTr9h-Gd83srkzeYrxnFvE9YgB-FOFNJZ2a48vBhHbvT3HF7RecV_Se73";
+
+            //SE CREA EL MENSAJE QUE CONTENDRA LA NOTIFICACION
+            var message = new FirebaseAdmin.Messaging.Message()
+            {
+                Notification = new Notification()
+                {
+                    Title = txtTitulo.Text,
+                    Body = txtMensaje.Text,
+                },
+                Token = registrationToken,
+            };
+
+            //ENVIAR EL MENSAJE AL TOKEN
+            string response = await FirebaseMessaging.DefaultInstance.SendAsync(message);
+
+            //RESPUESTA DE LA PETICION CON EL ID DEL MENSAJE ENVIADO
+            MessageBox.Show("Successfully sent message: " + response);
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            btnPeticion_ClickAsync();
+        }
+    }
+}
